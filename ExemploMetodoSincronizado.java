4e04f93d1d362fa33774713d0748497eef6c023b
Barretuino Tecnologia <41486280+barretuino@users.noreply.github.com>
2023-04-06 19:36:42 -0300

    Create ExemploMetodoSincronizado.java

---
+++ ExemploMetodoSincronizado.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExemploMetodoSincronizado {
+
+	private static int varCompartilhada = 0;
+	private static final Integer QUANTIDADE = 10000;
+	private static final List<Integer> VALORES = new ArrayList<>();
+
+	public static void main(String[] args) {
+
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < QUANTIDADE; i++) {
+					incrementaEAdd();
+				}
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < QUANTIDADE; i++) {
+					incrementaEAdd();
+				}
+			}
+		});
+
+		Thread t3 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < QUANTIDADE; i++) {
+					incrementaEAdd();
+				}
+			}
+		});
+
+		t1.start();
+		t2.start();
+		t3.start();
+
+		try {
+			t1.join();
+			t2.join();
+			t3.join();
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+
+		int soma = 0;
+		for (Integer valor : VALORES) {
+			soma += valor;
+		}
+		System.out.println("Soma: " + soma);
+	}
+
+	private synchronized static void incrementaEAdd() {
+		VALORES.add(++varCompartilhada);
+	}
+
+}
